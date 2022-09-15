1ca0014da9ece50fd223f12917f3958b4df3d6ad
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2022-09-15 13:37:50 +0600

    static pages wetup and routing

--- blog/resources/views/website/contact.blade.php
+++ blog/resources/views/website/contact.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.website')
-@section('content')    
-    <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('{{ asset('website') }}/images/img_4.jpg');">
+
+@section('content')
+    
+    <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('{{asset('website')}}/images/img_4.jpg');">
       <div class="container">
         <div class="row same-height justify-content-center">
           <div class="col-md-12 col-lg-10">
@@ -13,34 +15,41 @@
       </div>
     </div>
     
+    
     <div class="site-section bg-light">
       <div class="container">
         <div class="row">
           <div class="col-md-7 mb-5">
-            <form action="{{ route('website.contact') }}" method="post" class="p-5 bg-white">
-              @csrf 
-              @include('includes.errors')
-              @if(Session::has('message-send'))
-                <div class="alert alert-success">{{ Session::get('message-send') }}</div>
-              @endif
+
+            
+
+            <form action="#" class="p-5 bg-white">
+             
+
               <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="text-black" for="fname">Name</label>
-                  <input type="text" id="name" name="name" class="form-control" placeholder="Name">
+                <div class="col-md-6 mb-3 mb-md-0">
+                  <label class="text-black" for="fname">First Name</label>
+                  <input type="text" id="fname" class="form-control">
+                </div>
+                <div class="col-md-6">
+                  <label class="text-black" for="lname">Last Name</label>
+                  <input type="text" id="lname" class="form-control">
                 </div>
               </div>
 
               <div class="row form-group">
+                
                 <div class="col-md-12">
                   <label class="text-black" for="email">Email</label> 
-                  <input type="email" id="email" name="email" class="form-control" placeholder="Email">
+                  <input type="email" id="email" class="form-control">
                 </div>
               </div>
 
               <div class="row form-group">
+                
                 <div class="col-md-12">
                   <label class="text-black" for="subject">Subject</label> 
-                  <input type="subject" id="subject" name="subject" class="form-control" placeholder="Subject">
+                  <input type="subject" id="subject" class="form-control">
                 </div>
               </div>
 
@@ -64,13 +73,13 @@
             
             <div class="p-4 mb-3 bg-white">
               <p class="mb-0 font-weight-bold">Address</p>
-              <p class="mb-4">{{ $setting->address }}</p>
+              <p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
 
               <p class="mb-0 font-weight-bold">Phone</p>
-              <p class="mb-4"><a href="#">{{ $setting->phone }}</a></p>
+              <p class="mb-4"><a href="#">+1 232 3235 324</a></p>
 
               <p class="mb-0 font-weight-bold">Email Address</p>
-              <p class="mb-0"><a href="#">{{ $setting->email }}</a></p>
+              <p class="mb-0"><a href="#">youremail@domain.com</a></p>
 
             </div>
 
@@ -78,4 +87,7 @@
         </div>
       </div>
     </div>
+    
+    
+
 @endsection
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -22,5 +22,18 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/', function(){
-    return view('layouts/website');
+    return view('website.home');
+});
+
+Route::get('/about', function(){
+    return view('website.about');
+});
+Route::get('/category', function(){
+    return view('website.category');
+});
+Route::get('/contact', function(){
+    return view('website.contact');
+});
+Route::get('/post', function(){
+    return view('website.post');
 });
